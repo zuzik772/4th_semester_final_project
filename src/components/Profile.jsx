c13e1c506208c246dc283cd13f9dc2b4d18dd197
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-12-04 16:36:20 +0100

    add icons

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,7 @@
 export default function Profile() {
   return (
-    <li className="bg-fadedAccent w-16 h-16 rounded-full flex items-center justify-center">
-      <span className="font-bold text-3xl">AS</span>
+    <li className="bg-fadedAccent w-12 h-12 rounded-full flex items-center justify-center cursor-pointer">
+      <span className="font-bold text-xl">AS</span>
     </li>
   );
 }
