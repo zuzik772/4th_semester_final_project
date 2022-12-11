e0381453c1271904d9e236886e7bb6fda782962b
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-12-11 12:32:08 +0100

    change styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,45 +5,50 @@ import RequiredIcon from "../components/icons/RequiredIcon";
 export default function Login() {
   return (
     <div className="grid sm:grid-cols-2 bg-light h-screen">
-      <div className="flex flex-col items-center justify-center gap-6 mb-4 sm:mb-0">
-        <img
-          src={darkLogo}
-          alt="Beta Boulders logo"
-          className="w-60 sm:w-80 mb-2 sm:mb-10 lg:20"
-        ></img>
-        <div className="w-60 sm:w-80">
-          <h1 className="uppercase font-semibold text-xl text-left">Log in</h1>
-          <span className="text-xs">Welcome back! Please enter your details</span>
+      <div className="flex flex-col justify-evenly items-center">
+        <img src={darkLogo} alt="Beta Boulders logo" className="w-40 sm:w-60"></img>
+        <div className="flex flex-col items-center justify-center gap-8 mb-4 sm:mb-0">
+          <div className="w-60 sm:w-80">
+            <h1 className="uppercase font-semibold text-2xl text-left">Log in</h1>
+            <span className="text-sm">Welcome back! Please enter your details</span>
+          </div>
+          <form className="flex flex-col gap-6 w-60 sm:w-80">
+            <div>
+              <label for="username" className="flex text-sm">
+                Username
+                <RequiredIcon />
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Put your password here"
+                required
+                className="pl-0 pb-0 outline-none bg-light w-full border-x-0 border-t-0 border-b-gray-400 rounded-none focus:border-t-0 focus:border-x-0 focus:ring-0 dark:focus:ring-0 focus:border-accent"
+              />
+            </div>
+            <div>
+              <label for="password" className="flex text-sm">
+                Password
+                <RequiredIcon />
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="And your password here"
+                required
+                className="pl-0 pb-0 outline-none bg-light w-full border-x-0 border-t-0 border-b-gray-400 rounded-none focus:border-t-0 focus:border-x-0 focus:ring-0 dark:focus:ring-0 focus:border-accent"
+              />
+            </div>
+          </form>
+          <div className="flex flex-col gap-1 items-center">
+            <button type="submit" className="bg-accent text-white rounded-lg px-4 py-1 h-fit w-40">
+              Sign in
+            </button>
+            <span className="text-xs opacity-70">Have a good working day!</span>
+          </div>
         </div>
-        <form className="flex flex-col gap-0.5 w-60 sm:w-80">
-          <div>
-            <label for="username" className="flex pt-2 text-xs">
-              Username
-              <RequiredIcon />
-            </label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              className="font-bold text-md outline-none text-dark bg-darkerLight w-full border-light rounded-md  focus:ring-0 dark:focus:ring-0 focus:border-accent"
-            />
-          </div>
-          <div>
-            <label for="password" className="flex pt-2 text-xs">
-              Password
-              <RequiredIcon />
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="font-bold text-md outline-none text-dark bg-darkerLight w-full border-light rounded-md  focus:ring-0 dark:focus:ring-0 focus:border-accent"
-            />
-          </div>
-        </form>
-        <button type="submit" className="bg-accent text-white rounded-lg px-4 py-1 h-fit w-40">
-          Sign in
-        </button>
       </div>
       <div>
         <img
