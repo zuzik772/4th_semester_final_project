15328bc6bd5a6b243fc6f87e031660f376796a18
aszulc97 <agataszulc97@gmail.com>
2022-12-14 14:39:07 +0100

    inventory location filter

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,14 +1,23 @@
+import { useRef } from "react";
 import "../global.css";
 export default function Dropdown(props) {
+  const locationRef = useRef();
+
+  function changeLocation() {
+    props.getLocation(locationRef.current.value)
+  }
+
   return (
     <div className="flex gap-2 items-center">
       <label htmlFor="gym" className="text-light font-normal hidden md:block">
         Location:
       </label>
       <select
+        ref={locationRef}
         id="gym"
         className="hidden md:block minimal rounded-xl py-1 bg-light font-medium border-2 outline-none border-none cursor-pointer "
         defaultValue={props.location}
+        onChange={changeLocation}
         >
         <option value="west" >
           Beta West
