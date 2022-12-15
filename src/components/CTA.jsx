02f5757f733a73428f33daf319a76491b7603528
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-12-15 17:14:06 +0100

    last styling

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,6 +1,9 @@
 export default function CTA(props) {
   return (
-    <button onClick={props.handleCTA} className="bg-accent text-white rounded-lg px-4 py-1 h-fit">
+    <button
+      onClick={props.handleCTA}
+      className="bg-accent text-white rounded-lg px-4 py-1 mb-4 sm:mb-0 h-fit"
+    >
       {props.title}
     </button>
   );
