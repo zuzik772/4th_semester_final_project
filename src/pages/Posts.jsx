96d8c9cc42dfdca9f5d2c664ff9c5618fc34de96
aszulc97 <agataszulc97@gmail.com>
2022-12-15 14:16:22 +0100

    fetch works

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -5,7 +5,29 @@ import matcha3 from "../img/matcha3.webp";
 import SeenByPeople from "../components/icons/SeenByPeople";
 import Attachment from "../components/icons/Attachment";
 import CTA from "../components/CTA";
+import { useState, useEffect, useRef } from "react";
 export default function Posts(props) {
+  const [postsArray, setPostsArray] = useState([]);
+  const postRef = useRef();
+
+  const url = "https://louisiana-2c6b.restdb.io/rest/posts?sort=date";
+  const options = {
+    headers: {
+      "x-apikey": "63925f89f43a573dae0953ee",
+    },
+  };
+
+  useEffect(() => {
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        setPostsArray(data);
+      });
+    // eslint-disable-next-line
+  }, []);
+
+  function post() {}
+
   return (
     <main className="w-full lg:w-3/5 p-2 sm:p-6 sm:pl-12 block lg:grid gap-6">
       <div>
@@ -18,6 +40,7 @@ export default function Posts(props) {
       <div className="flex flex-col gap-2 bg-light p-2 sm:p-5 mb-5 lg:mb-0">
         <h3 className="text-lg font-semibold">Write new post</h3>
         <textarea
+          ref={postRef}
           name="post"
           id="post"
           cols="30"
@@ -30,7 +53,7 @@ export default function Posts(props) {
             <Attachment />
             <span className="font-medium text-sm">add attachment</span>
           </div>
-          <CTA title="Add post" />
+          <CTA title="Add post" handleCTA={post} />
         </div>
       </div>
       {/* new posts */}
@@ -48,8 +71,9 @@ export default function Posts(props) {
           </div>
           <h4 className="text-md font-medium mt-2">Pilates</h4>
           <p>
-            Now we offer pilates as a part of our flexibility program. The classes will take place
-            every Tuesday and Saturday at 19:00 in the yoga gym.
+            Now we offer pilates as a part of our flexibility program. The
+            classes will take place every Tuesday and Saturday at 19:00 in the
+            yoga gym.
           </p>
           <p>Customers can reserve a spot online, same as for yoga.</p>
 
@@ -59,6 +83,27 @@ export default function Posts(props) {
           </div>
         </div>
       </div>
+
+      {postsArray.map((post) => (
+        <div className="mt-6" key={post._id}>
+          <div className="flex flex-col gap-2 bg-light p-2 sm:p-5 col-span-3 mb-5 lg:mb-0">
+            <div className="flex gap-2 items-center">
+              <span className="bg-fadedBlue w-10 h-10 p-2 rounded-full flex items-center justify-center cursor-pointerfont-bold text-md font-medium cursor-pointer">
+                {post.initials}
+              </span>
+              <div className="flex flex-col ">
+                <span className="font-medium">{post.user}</span>
+                <span className="font-light text-xs">{post.date}</span>
+              </div>
+            </div>
+            <p>{post.text}</p>
+            <div className="flex gap-1 items-center justify-end">
+              <SeenByPeople />
+              <span className="font-medium text-sm">10</span>
+            </div>
+          </div>
+        </div>
+      ))}
       {/* old posts */}
       <div className="mt-6">
         <h3 className="text-lg font-semibold">Old Posts</h3>
@@ -72,7 +117,9 @@ export default function Posts(props) {
               <span className="font-light text-xs">22. nov 2022 9:58</span>
             </div>
           </div>
-          <h4 className="text-md font-medium mt-2">Now we offer matcha latte</h4>
+          <h4 className="text-md font-medium mt-2">
+            Now we offer matcha latte
+          </h4>
 
           <ol className="list-decimal pl-4 text-sm leading-6">
             <li>Mix 1 tablespoon of matcha in yellow cup</li>
@@ -83,16 +130,28 @@ export default function Posts(props) {
           </ol>
           <div className="grid grid-cols-2 sm:grid-cols-3 gap-2 justify-center">
             <figure>
-              <img src={matcha1} alt="instruction of how to make matcha" className="w-fit" />
+              <img
+                src={matcha1}
+                alt="instruction of how to make matcha"
+                className="w-fit"
+              />
               <figcaption>step 1</figcaption>
             </figure>
             <figure>
-              <img src={matcha2} alt="instruction of how to make matcha" className="w-fit" />
+              <img
+                src={matcha2}
+                alt="instruction of how to make matcha"
+                className="w-fit"
+              />
               <figcaption>step 2</figcaption>
             </figure>
 
             <figure>
-              <img src={matcha3} alt="instruction of how to make matcha" className="w-fit" />
+              <img
+                src={matcha3}
+                alt="instruction of how to make matcha"
+                className="w-fit"
+              />
               <figcaption>step 3</figcaption>
             </figure>
           </div>
